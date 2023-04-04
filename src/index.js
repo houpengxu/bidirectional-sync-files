ea22b9c30cd673b3047aa2dfb8a1498aa6b293a8
矦倗諿 <houpengxu@qq.com>
2023-04-05 00:23:49 +0800

    feat: 自动创建to文件

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const chokidar = require('chokidar');
 
 let config = null
 const customPath = process.argv[2]
-const userHomeConfig = os.homedir() + '/.bi-sync-files.json';
+const userHomeConfig = os.homedir() + '/.bidirectional-sync-files.json';
 // 自定义路径
 if (customPath && fse.pathExistsSync(customPath)) {
   config = require(customPath)
@@ -15,7 +15,7 @@ if (customPath && fse.pathExistsSync(customPath)) {
   config = require(userHomeConfig)
 } else {
   // 程序运行的相对路径
-  console.log('请在用户目录设置.bi-sync-files.json')
+  console.log('请在用户目录设置.bidirectional-sync-files.json')
   config = require('./config.json')
 }
 
@@ -30,8 +30,8 @@ const intervalSyncTime = 1000 * 60 * 60
 
 init();
 
+// 入口
 function init() {
-
   // 根据相对路径获取出绝对路径
   config.syncs = config.syncs.map(item => {
     if (item.from.startsWith('../') || item.from.startsWith('./')) {
@@ -42,37 +42,21 @@ function init() {
     }
     return item
   })
-
-
-  // 检测文件或文件夹是否存在
-  let allFileExist = true
-  for (const item of config.syncs) {
-    const { from, to, ignored } = item;
-    // 注意点：不能自动创建，因为一旦可以自动创建了，新创建的文件就是最新的文件了，就会覆盖本来的文件。
-    if (!fse.pathExistsSync(from)) {
-      log(`path "${from}" does not exist.`)
-      allFileExist = false
-    }
-    if (!fse.pathExistsSync(to)) {
-      log(`path "${to}" does not exist.`)
-      allFileExist = false
-    }
-  }
-  if (allFileExist) {
-    start()
-  }
+  log(JSON.stringify(config.syncs))
+  start()
 }
 
 
 // 开始
 function start() {
+  log('start')
   // 监听源路径的文件或文件夹变化
   for (const item of config.syncs) {
     const { from, to, ignored } = item;
     chokidar.watch(from, {
       ignored: ignored
     }).on('all', async (event, path) => {
-      log(`File ${path} has been ${event}.`);
+      log(`watch: file ${path} has been ${event}.`);
       await sync(from, to, ignored);
     });
   }
@@ -86,7 +70,7 @@ function start() {
         chokidar.watch(to, {
           ignored: ignored
         }).on('all', async (event, path) => {
-          log(`File ${path} has been ${event}.`);
+          log(`watch: file ${path} has been ${event}.`);
           await sync(to, from, ignored);
         });
       }
@@ -95,6 +79,7 @@ function start() {
 
   // 定时同步
   setInterval(() => {
+    log('interval from->to：')
     if (syncFileList.size > 0) return
     for (const item of config.syncs) {
       const { from, to, ignored } = item;
@@ -102,6 +87,7 @@ function start() {
     }
 
     const interval = setInterval(() => {
+      log('interval to->from：')
       if (syncFileList.size > 0) return
       clearInterval(interval)
       for (const item of config.syncs) {
@@ -115,53 +101,69 @@ function start() {
 
 
 // 同步
-async function sync(fromPath, toPath, ignored = []) {
-  if (syncFileList.has(fromPath) || syncFileList.has(toPath)) {
+async function sync(from, to, ignored = []) {
+  // 如果文件在当前同步列表中return
+  if (syncFileList.has(from) || syncFileList.has(to)) {
     return
   }
-  syncFileList.add(fromPath)
-  syncFileList.add(toPath)
-  // 判断源路径和目标路径是否存在
-  const fromExists = await fse.pathExists(fromPath);
-  const toExists = await fse.pathExists(toPath);
-  if (!fromExists) {
-    log(`Source path "${fromPath}" does not exist.`);
-    return;
+  // 如果from文件不存在则return
+  if (!fse.pathExistsSync(from)) {
+    log(`noExist: ${from}`)
+    return
   }
-  if (!toExists) {
-    log(`Destination path "${toPath}" does not exist.`);
-    return;
+  // 判断from是文件还是文件夹，来自动创建，标记问新路径不进行修改时间比对
+  let toIsNewFile = false
+  if (!fse.pathExistsSync(to)) {
+    toIsNewFile = true
+    const stats = fse.statSync(from)
+    if (stats.isDirectory()) {
+      fse.ensureDirSync(to)
+    } else {
+      fse.ensureFileSync(to)
+    }
   }
 
-  if (await compareFileTime(fromPath, toPath)) {
-    // 拷贝源路径到目标路径
-    await fse.copy(fromPath, toPath, {
-      overwrite: true,
-      filter: (src, dest) => {
-        // 忽略指定的文件或文件夹
-        for (const ignore of ignored) {
-          if (src.includes(ignore)) {
-            return false;
+  if (compareFileTime(from, to) || toIsNewFile) {
+    // 添加到同步列表
+    syncFileList.add(from)
+    syncFileList.add(to)
+
+    try {
+      // 拷贝源路径到目标路径
+      log(`copy: ${from} -> ${to}`)
+      await fse.copy(from, to, {
+        overwrite: true,
+        filter: (src) => {
+          // 忽略指定的文件或文件夹
+          for (const ignore of ignored) {
+            if (src.includes(ignore)) {
+              return false;
+            }
           }
+          return true;
         }
-        return true;
-      }
-    });
+      });
+    } catch (error) {
+      log(`error copy: ${error.toString()}`)
+    }
+    setTimeout(() => {
+      syncFileList.delete(from)
+      syncFileList.delete(to)
+    }, lockSyncTime);
   }
-  setTimeout(() => {
-    syncFileList.delete(fromPath)
-    syncFileList.delete(toPath)
-  }, lockSyncTime);
 }
 
 // 比较两个文件的修改时间
-async function compareFileTime(path1, path2) {
-  const stat1 = await fse.stat(path1);
-  const stat2 = await fse.stat(path2);
+function compareFileTime(path1, path2) {
+  const stat1 = fse.statSync(path1);
+  const stat2 = fse.statSync(path2);
   // 返回 true 表示第一个文件的修改时间晚于第二个文件的修改时间
   return stat1.mtime > stat2.mtime;
 }
 
+// 日志
 function log(msg) {
-  console.log(`${new Date().toISOString()}: ${msg}`)
+  const date = new Date();
+  const milliseconds = date.getMilliseconds();
+  console.log(`${date.toLocaleString()}.${milliseconds}: ${msg}`)
 }
